8126afb4b70cd2f0dacdf87653b5185e419f9ab0
ed <cabrale234@gmail.com>
2022-11-04 14:49:29 -0400

    getUserInfor

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,13 +3,15 @@ const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
+// Generate Token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
 };
 
 // Register User
 const registerUser = asyncHandler(async (req, res) => {
-  let { name, email, password } = req.body;
+  const { name, email, password } = req.body;
+
   // Validation
   if (!name || !email || !password) {
     res.status(400);
@@ -19,44 +21,48 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Password must be up to 6 characters");
   }
+
+  // Check if user email already exists
   const userExists = await User.findOne({ email });
+
   if (userExists) {
     res.status(400);
-    throw new Error("Email has already been used");
+    throw new Error("Email has already been registered");
   }
 
   // Create new user
-
   const user = await User.create({
     name,
     email,
     password,
   });
 
-  // Generate Token
-  const token = generateToken(req.body._id);
+  //   Generate Token
+  const token = generateToken(user._id);
 
   // Send HTTP-only cookie
   res.cookie("token", token, {
     path: "/",
-    httpOnly: true.valueOf,
+    httpOnly: true,
     expires: new Date(Date.now() + 1000 * 86400), // 1 day
     sameSite: "none",
     secure: true,
   });
 
   if (user) {
+    const { _id, name, email, photo, phone, bio } = user;
     res.status(201).json({
-      _id: user.id,
-      name: user.name,
-      email: user.email,
-      photo: user.photo,
-      bio: user.bio,
+      _id,
+      name,
+      email,
+      photo,
+      phone,
+      bio,
       token,
     });
   } else {
     res.status(400);
-    throw new Error("invalid user data");
+    throw new Error("Invalid user data");
   }
 });
 
@@ -66,26 +72,28 @@ const loginUser = asyncHandler(async (req, res) => {
 
   // Validate Request
   if (!email || !password) {
-    res.send(400);
+    res.status(400);
     throw new Error("Please add email and password");
   }
+
   // Check if user exists
   const user = await User.findOne({ email });
+
   if (!user) {
-    res.send(400);
+    res.status(400);
     throw new Error("User not found, please signup");
   }
 
   // User exists, check if password is correct
   const passwordIsCorrect = await bcrypt.compare(password, user.password);
 
-  // Generate Token
-  const token = generateToken(req.body._id);
+  //   Generate Token
+  const token = generateToken(user._id);
 
   // Send HTTP-only cookie
   res.cookie("token", token, {
     path: "/",
-    httpOnly: true.valueOf,
+    httpOnly: true,
     expires: new Date(Date.now() + 1000 * 86400), // 1 day
     sameSite: "none",
     secure: true,
@@ -93,7 +101,7 @@ const loginUser = asyncHandler(async (req, res) => {
 
   if (user && passwordIsCorrect) {
     const { _id, name, email, photo, phone, bio } = user;
-    res.json({
+    res.status(200).json({
       _id,
       name,
       email,
@@ -108,21 +116,36 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
-// Logout Users
+// Logout User
 const logout = asyncHandler(async (req, res) => {
   res.cookie("token", "", {
     path: "/",
     httpOnly: true,
-    expires: new Date(0), //expires the cookie immediately
+    expires: new Date(0),
     sameSite: "none",
     secure: true,
   });
-  return res.status(200).json({ message: "sucessfully logged out" });
+  return res.status(200).json({ message: "Successfully Logged Out" });
 });
 
-// Get user data
+// Get User Data
 const getUser = asyncHandler(async (req, res) => {
-  res.send("Get User Data");
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    const { _id, name, email, photo, phone, bio } = user;
+    res.status(200).json({
+      _id,
+      name,
+      email,
+      photo,
+      phone,
+      bio,
+    });
+  } else {
+    res.status(400);
+    throw new Error("User Not Found");
+  }
 });
 
 module.exports = {
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,15 +1,16 @@
 const express = require("express");
+const router = express.Router();
 const {
   registerUser,
   loginUser,
   logout,
   getUser,
 } = require("../controllers/userController");
-const router = express.Router();
+const protect = require("../middleWare/authMiddleWare");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", logout);
-router.get("/getuser", getUser);
+router.get("/getuser", protect, getUser);
 
 module.exports = router;
